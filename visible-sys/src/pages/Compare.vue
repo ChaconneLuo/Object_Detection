af7e26362832c80f5f47bd1d3e99a71ee4bf50f6
ChaconneLuo <asgardannila@gmail.com>
2024-05-20 10:40:38 +0800

    chore: level

--- visible-sys/src/pages/Compare.vue
+++ visible-sys/src/pages/Compare.vue
@@ -3,16 +3,13 @@
     <el-row :gutter="2">
       <el-col :span="12">
         <span>Algorithm: </span>
-        <el-select v-model="algorithmValue" placeholder="Select" size="large" style="width: 240px" :teleported="false"
-          popper-class="select_popper">
-          <el-option v-for="item in algorithmOptions" :key="item.algorithm" :label="item.algorithm"
-            :value="item.algorithm" />
+        <el-select v-model="algorithmValue" placeholder="Select" size="large" style="width: 240px" :teleported="false" popper-class="select_popper">
+          <el-option v-for="item in algorithmOptions" :key="item.algorithm" :label="item.algorithm" :value="item.algorithm" />
         </el-select>
       </el-col>
       <el-col :span="12">
         <span>Model: </span>
-        <el-select v-model="modelValue" placeholder="Select" size="large" style="width: 240px" :teleported="false"
-          popper-class="select_popper">
+        <el-select v-model="modelValue" placeholder="Select" size="large" style="width: 240px" :teleported="false" popper-class="select_popper">
           <el-option v-for="item in modelOptions" :key="item" :label="item" :value="item" />
         </el-select>
       </el-col>
@@ -36,11 +33,14 @@
       </el-col>
     </el-row>
     <el-row>
-      <el-col :span="24" class="flex flex-col">
-        检测出fire级别火势：{{ checked.filter((item) => item[4] === 'fire' && getLength(item) > 70).length }}
-        检测出fire_middle级别火势：{{ checked.filter((item) => item[4] === 'fire' && getLength(item) <= 70).length }}
-          检测出fire_small级别火势：{{ checked.filter((item) => item[4] === 'fire_small').length }}
-          检测出火苗个数：{{ checked.length }}
+      <el-col :span="24">
+        <div class="flex flex-col">
+          <span>检测出fire级别火势: {{ checked.filter((item) => item[4] === 'fire' && getLength(item) > 70).length }}</span>
+          <span>检测出fire_middle级别火势: {{ checked.filter((item) => item[4] === 'fire' && getLength(item) <= 70).length }}</span>
+          <span>检测出fire_small级别火势: {{ checked.filter((item) => item[4] === 'fire_small').length }}</span>
+          <span>检测出火苗个数: {{ checked.filter((item) => item[4] !== 'smoke').length }}</span>
+          <span>检测出烟雾个数: {{ checked.filter((item) => item[4] === 'smoke').length }}</span>
+        </div>
       </el-col>
     </el-row>
     <el-row>
@@ -51,92 +51,90 @@
         <canvas ref="imgRef" class="w-100%"></canvas>
       </el-col>
     </el-row>
-
   </div>
 </template>
 <script setup lang="ts">
-import { invoke, convertFileSrc } from '@tauri-apps/api/tauri';
-import { open } from "@tauri-apps/api/dialog"
-import { ref, onMounted, watch } from 'vue';
+import { invoke, convertFileSrc } from '@tauri-apps/api/tauri'
+import { open } from '@tauri-apps/api/dialog'
+import { ref, onMounted, watch } from 'vue'
 
 type Option = {
-  algorithm: string;
-  model: string[];
+  algorithm: string
+  model: string[]
 }
 
-const algorithmOptions = ref<Option[]>([]);
-const algorithmValue = ref<string>('');
-const modelOptions = ref<string[]>([]);
-const modelValue = ref<string>('');
-const imgLink = ref<string>('');
-const imgRef = ref();
-let imgPath = '';
+const algorithmOptions = ref<Option[]>([])
+const algorithmValue = ref<string>('')
+const modelOptions = ref<string[]>([])
+const modelValue = ref<string>('')
+const imgLink = ref<string>('')
+const imgRef = ref()
+let imgPath = ''
 const checked = ref<any[]>([])
-const iou = ref<number>(0.5);
-const conf = ref<number>(0.5);
+const iou = ref<number>(0.5)
+const conf = ref<number>(0.5)
 
 async function getAlgorithms() {
-  const response: string = await invoke('get_algorithms');
-  algorithmOptions.value = JSON.parse(response);
-  algorithmValue.value = algorithmOptions.value[0].algorithm;
-  modelOptions.value = algorithmOptions.value[0].model;
+  const response: string = await invoke('get_algorithms')
+  algorithmOptions.value = JSON.parse(response)
+  algorithmValue.value = algorithmOptions.value[0].algorithm
+  modelOptions.value = algorithmOptions.value[0].model
 }
 
 const useModel = async () => {
   // handledImg.value = ''
   if (imgLink.value) {
-    invoke('detect',
-      { algorithm: algorithmValue.value, model: modelValue.value, img: imgPath, iouValue: iou.value, confValue: conf.value }).then((res) => {
-        // handledImg.value = convertFileSrc((res as string).slice(1, -1))
-        console.log(JSON.parse(res))
-        checked.value = JSON.parse(res)
-        draw_image_and_boxes(imgPath, JSON.parse(res))
-      })
+    invoke('detect', { algorithm: algorithmValue.value, model: modelValue.value, img: imgPath, iouValue: iou.value, confValue: conf.value }).then((res) => {
+      // handledImg.value = convertFileSrc((res as string).slice(1, -1))
+      console.log(JSON.parse(res))
+      checked.value = JSON.parse(res)
+      draw_image_and_boxes(imgPath, JSON.parse(res))
+    })
   }
 }
 
 const openFile = () => {
   open({
-    multiple: false
+    multiple: false,
   }).then((file) => {
-    imgPath = file as string;
+    imgPath = file as string
     imgLink.value = convertFileSrc(file as string)
     draw_image_and_boxes(imgPath, [])
   })
 }
 const draw_image_and_boxes = (file, boxes: any[]) => {
   const img = new Image()
-  img.src = convertFileSrc(file as string);
+  img.src = convertFileSrc(file as string)
   img.onload = () => {
-    const canvas = document.querySelector("canvas")!;
-    canvas.width = img.width;
-    canvas.height = img.height;
-    const ctx = canvas.getContext("2d")!;
-    ctx.drawImage(img, 0, 0);
-    ctx.strokeStyle = "#00FF00";
-    ctx.lineWidth = 3;
-    ctx.font = "18px serif";
+    const canvas = document.querySelector('canvas')!
+    canvas.width = img.width
+    canvas.height = img.height
+    const ctx = canvas.getContext('2d')!
+    ctx.drawImage(img, 0, 0)
+    ctx.strokeStyle = '#00FF00'
+    ctx.lineWidth = 3
+    ctx.font = '18px serif'
     boxes.forEach(([x1, y1, x2, y2, label]: [number, number, number, number, string]) => {
-      ctx.strokeRect(x1, y1, x2 - x1, y2 - y1);
-      ctx.fillStyle = "#00ff00";
-      const width = ctx.measureText(label).width;
-      ctx.fillRect(x1, y1, width + 10, 25);
-      ctx.fillStyle = "#000000";
-      ctx.fillText(label, x1, y1 + 18);
-    });
+      ctx.strokeRect(x1, y1, x2 - x1, y2 - y1)
+      ctx.fillStyle = '#00ff00'
+      const width = ctx.measureText(label).width
+      ctx.fillRect(x1, y1, width + 10, 25)
+      ctx.fillStyle = '#000000'
+      ctx.fillText(label, x1, y1 + 18)
+    })
   }
 }
 
 const getLength = (position: number[]) => {
-  return Math.sqrt(Math.pow(position[2] - position[0], 2) + Math.pow(position[3] - position[1], 2));
+  return Math.sqrt(Math.pow(position[2] - position[0], 2) + Math.pow(position[3] - position[1], 2))
 }
 
 onMounted(() => {
-  getAlgorithms();
+  getAlgorithms()
 })
 
 watch(algorithmValue, async (newValue) => {
-  modelOptions.value = algorithmOptions.value.find((item) => item.algorithm === newValue)?.model || [];
+  modelOptions.value = algorithmOptions.value.find((item) => item.algorithm === newValue)?.model || []
   modelValue.value = modelOptions.value[0]
 })
 </script>
